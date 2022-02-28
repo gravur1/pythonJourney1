bb8042156d4e6919cd87027e7258fb3f3edd837f
gravur <tiagometur@gmail.com>
2022-02-28 12:09:15 -0500

    Adding module creation tips

--- modules.py
+++ modules.py
@@ -1,8 +1,29 @@
+from setuptools import setup
 import math
 import os
 
+########
+### simple modules
+
+
 #print("Pi: ", math.pi)
 
-user = os.environ["USER"]
-path = os.environ["PATH"]
-print("User: ", user + "\nPath: " + path)
+#user = os.environ["USER"]
+#path = os.environ["PATH"]
+#print("User: ", user + "\nPath: " + path)
+
+#setup(
+#    name = 'calculator',
+#   version = '1.0',
+#    description = 'My new python module',
+#    author = 'gravur',
+#    py_modules = ['calculator']
+#)
+
+#once the code above is executed, a set of files and folders are generated to allow the module to be used by anyone
+#pip install calculator-1.0.tar.gz - Installing the module
+#import calculator
+#calculator.function()
+
+
+
